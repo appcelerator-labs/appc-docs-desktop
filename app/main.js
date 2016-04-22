1463b2287b2041b7fd993158f45aeb30a99b0be3
Manoj Kumar <mano.mykingdom@gmail.com>
2016-04-23 01:25:57 +0530

    updated main window

--- app/main.js
+++ app/main.js
@@ -1,187 +1,201 @@
-var app = require('app'); // Module to control application life.
-var BrowserWindow = require('browser-window'); // Module to create native browser window.
-var Menu = require('menu');
+const Electron = require('electron');
+const app = Electron.app; // Module to control application life.
+const Menu = Electron.Menu; //Module to control application menu.
+const BrowserWindow = Electron.BrowserWindow; // Module to create native browser window.
+const crashReporter = Electron.crashReporter; // Report crashes to our server.
 
-// Report crashes to our server.
-require('crash-reporter').start();
+/*crashReporter.start({
+ productName: 'AppC-Docs',
+ companyName: 'Appcelerator',
+ submitURL: 'https://your-domain.com/url-to-submit' //required parameter now, seems electron no longer monitors crash itself
+ });*/
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+/**
+ * Keep a global reference of the window object, if you don't, the window will
+ * be closed automatically when the JavaScript object is garbage collected.
+ */
 var mainWindow = null;
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit();
-  }
+    /**
+     * On OS X it is common for applications and their menu bar
+     * to stay active until the user quits explicitly with Cmd + Q
+     */
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
+/**
+ * This method will be called when Electron has finished
+ * initialization and is ready to create browser windows.
+ */
 app.on('ready', function () {
 
-  var template = [{
-    label: 'Edit',
-    submenu: [{
-      label: 'Undo',
-      accelerator: 'CmdOrCtrl+Z',
-      role: 'undo'
+    var template = [{
+        label: 'Edit',
+        submenu: [{
+            label: 'Undo',
+            accelerator: 'CmdOrCtrl+Z',
+            role: 'undo'
+        }, {
+            label: 'Redo',
+            accelerator: 'Shift+CmdOrCtrl+Z',
+            role: 'redo'
+        }, {
+            type: 'separator'
+        }, {
+            label: 'Cut',
+            accelerator: 'CmdOrCtrl+X',
+            role: 'cut'
+        }, {
+            label: 'Copy',
+            accelerator: 'CmdOrCtrl+C',
+            role: 'copy'
+        }, {
+            label: 'Paste',
+            accelerator: 'CmdOrCtrl+V',
+            role: 'paste'
+        }, {
+            label: 'Select All',
+            accelerator: 'CmdOrCtrl+A',
+            role: 'selectall'
+        }]
     }, {
-      label: 'Redo',
-      accelerator: 'Shift+CmdOrCtrl+Z',
-      role: 'redo'
+        label: 'View',
+        submenu: [{
+            label: 'Reload',
+            accelerator: 'CmdOrCtrl+R',
+            click: function (item, focusedWindow) {
+                if (focusedWindow) {
+                    focusedWindow.reload();
+                }
+            }
+        }, {
+            label: 'Toggle Full Screen',
+            accelerator: (function () {
+                if (process.platform === 'darwin') {
+                    return 'Ctrl+Command+F';
+                } else {
+                    return 'F11';
+                }
+            })(),
+            click: function (item, focusedWindow) {
+                if (focusedWindow) {
+                    focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+                }
+            }
+        }, {
+            label: 'Toggle Developer Tools',
+            accelerator: (function () {
+                if (process.platform === 'darwin') {
+                    return 'Alt+Command+I';
+                } else {
+                    return 'Ctrl+Shift+I';
+                }
+            })(),
+            click: function (item, focusedWindow) {
+                if (focusedWindow) {
+                    focusedWindow.toggleDevTools();
+                }
+            }
+        },]
     }, {
-      type: 'separator'
+        label: 'Window',
+        role: 'window',
+        submenu: [{
+            label: 'Minimize',
+            accelerator: 'CmdOrCtrl+M',
+            role: 'minimize'
+        }, {
+            label: 'Close',
+            accelerator: 'CmdOrCtrl+W',
+            role: 'close'
+        },]
     }, {
-      label: 'Cut',
-      accelerator: 'CmdOrCtrl+X',
-      role: 'cut'
-    }, {
-      label: 'Copy',
-      accelerator: 'CmdOrCtrl+C',
-      role: 'copy'
-    }, {
-      label: 'Paste',
-      accelerator: 'CmdOrCtrl+V',
-      role: 'paste'
-    }, {
-      label: 'Select All',
-      accelerator: 'CmdOrCtrl+A',
-      role: 'selectall'
-    }]
-  }, {
-    label: 'View',
-    submenu: [{
-      label: 'Reload',
-      accelerator: 'CmdOrCtrl+R',
-      click: function (item, focusedWindow) {
-        if (focusedWindow) {
-          focusedWindow.reload();
-        }
-      }
-    }, {
-      label: 'Toggle Full Screen',
-      accelerator: (function () {
-        if (process.platform === 'darwin') {
-          return 'Ctrl+Command+F';
-        } else {
-          return 'F11';
-        }
-      })(),
-      click: function (item, focusedWindow) {
-        if (focusedWindow) {
-          focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-        }
-      }
-    }, {
-      label: 'Toggle Developer Tools',
-      accelerator: (function () {
-        if (process.platform === 'darwin') {
-          return 'Alt+Command+I';
-        } else {
-          return 'Ctrl+Shift+I';
-        }
-      })(),
-      click: function (item, focusedWindow) {
-        if (focusedWindow) {
-          focusedWindow.toggleDevTools();
-        }
-      }
-    }, ]
-  }, {
-    label: 'Window',
-    role: 'window',
-    submenu: [{
-      label: 'Minimize',
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    }, {
-      label: 'Close',
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    }, ]
-  }, {
-    label: 'Help',
-    role: 'help',
-    submenu: [{
-      label: 'Online Documentation',
-      click: function () {
-        require('shell').openExternal('http://docs.appcelerator.com/platform/latest/');
-      }
-    }, {
-      label: 'Report an Issue...',
-      click: function () {
-        require('shell').openExternal('https://github.com/appcelerator-developer-relations/appc-docs-desktop/issues');
-      }
-    }]
-  }, ];
+        label: 'Help',
+        role: 'help',
+        submenu: [{
+            label: 'Online Documentation',
+            click: function () {
+                require('shell').openExternal('http://docs.appcelerator.com/platform/latest/');
+            }
+        }, {
+            label: 'Report an Issue...',
+            click: function () {
+                require('shell').openExternal('https://github.com/appcelerator-developer-relations/appc-docs-desktop/issues');
+            }
+        }]
+    },];
 
-  if (process.platform === 'darwin') {
-    var name = app.getName();
-    template.unshift({
-      label: name,
-      submenu: [{
-        label: 'About ' + name,
-        role: 'about'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Services',
-        role: 'services',
-        submenu: []
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Hide ' + name,
-        accelerator: 'Command+H',
-        role: 'hide'
-      }, {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        role: 'hideothers'
-      }, {
-        label: 'Show All',
-        role: 'unhide'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click: function () {
-          app.quit();
-        }
-      }, ]
+    if (process.platform === 'darwin') {
+        var name = app.getName();
+        template.unshift({
+            label: name,
+            submenu: [{
+                label: 'About ' + name,
+                role: 'about'
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Services',
+                role: 'services',
+                submenu: []
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Hide ' + name,
+                accelerator: 'Command+H',
+                role: 'hide'
+            }, {
+                label: 'Hide Others',
+                accelerator: 'Command+Shift+H',
+                role: 'hideothers'
+            }, {
+                label: 'Show All',
+                role: 'unhide'
+            }, {
+                type: 'separator'
+            }, {
+                label: 'Quit',
+                accelerator: 'Command+Q',
+                click: function () {
+                    app.quit();
+                }
+            },]
+        });
+        // Window menu.
+        template[3].submenu.push({
+            type: 'separator'
+        }, {
+            label: 'Bring All to Front',
+            role: 'front'
+        });
+    }
+
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+
+    // Create the browser window.
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 768
     });
-    // Window menu.
-    template[3].submenu.push({
-      type: 'separator'
-    }, {
-      label: 'Bring All to Front',
-      role: 'front'
+
+    // and load the index.html of the app.
+    mainWindow.loadURL('file://' + __dirname + '/docs/platform/latest/index.html');
+
+    // Open the DevTools.
+    mainWindow.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        /**
+         * Dereference the window object, usually you would store windows
+         * in an array if your app supports multi windows, this is the time
+         * when you should delete the corresponding element.
+         */
+        mainWindow = null;
     });
-  }
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(template));
-
-  // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1024,
-    height: 768
-  });
-
-  // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/docs/latest/index.html');
-
-  // Open the DevTools.
-  // mainWindow.openDevTools();
-
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null;
-  });
 });
