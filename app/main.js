d089d29cb71069485c2b0f837c53b1245f55a5d5
Manoj Kumar <mano.mykingdom@gmail.com>
2016-04-23 11:27:07 +0530

    commented dev tools

--- app/main.js
+++ app/main.js
@@ -186,7 +186,7 @@ app.on('ready', function () {
     mainWindow.loadURL('file://' + __dirname + '/docs/platform/latest/index.html');
 
     // Open the DevTools.
-    mainWindow.openDevTools();
+    // mainWindow.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
